26791d34884d5e9e19e11521ebb3e3b61ef49e0d
Fernando Calderon <fercalderon37@gmail.com>
2016-01-31 14:24:20 -0700

    Implementation of FernandoPalindromeAnalyzer

---
+++ Desktop/tdd_workshop-solution3/src/main/java/com/developersdelicias/palindrome/util/PalindromeComplexityCalculator.java
@@ -0,0 +1,5 @@
+package com.developersdelicias.palindrome.util;
+
+public interface PalindromeComplexityCalculator {
+	public PalindromeComplexity calculate(String palindrome) throws InvalidPalindromeException; 
+}
---
+++ Desktop/tdd_workshop-solution3/src/test/java/com/developersdelicias/palindrome/util/FernandoPalindromeAnalyzerTest.java
@@ -0,0 +1,12 @@
+package com.developersdelicias.palindrome.util;
+
+import org.junit.Before;
+
+public class FernandoPalindromeAnalyzerTest extends PalindromeAnalyzerTest {
+
+    @Before
+    public void setUp() {
+        analyzer = new FernandoPalindromeAnalyzer();
+    }
+
+}
